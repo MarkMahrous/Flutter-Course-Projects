7777ab36eaafe20e865f85a000ca72853dd98d7f
MarkMahrous <markmahrous012@gmail.com>
2024-04-16 03:28:47 +0200

    finalizing theming and adding dark mode

--- expenses_tracker/lib/custom_widgets/expenses_list/expenses_list.dart
+++ expenses_tracker/lib/custom_widgets/expenses_list/expenses_list.dart
@@ -13,6 +13,12 @@ class ExpensesList extends StatelessWidget {
     return ListView.builder(
       itemCount: registeredExpenses.length,
       itemBuilder: (ctx, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         onDismissed: (direction) {
           removeExpense(registeredExpenses[index]);
         },
