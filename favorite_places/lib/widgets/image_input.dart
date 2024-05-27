8279dba939109c9d1a4a982e8e07a12fb47671e2
MarkMahrous <markmahrous012@gmail.com>
2024-05-28 01:41:02 +0300

    Adding image to place feature

---
+++ favorite_places/lib/widgets/image_input.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageImput extends StatefulWidget {
+  const ImageImput({super.key});
+
+  @override
+  State<ImageImput> createState() => _ImageImputState();
+}
+
+class _ImageImputState extends State<ImageImput> {
+  File? _selectedImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      label: const Text('Add Image'),
+      icon: const Icon(Icons.camera),
+    );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
+    return Container(
+      // adding border
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+        ),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: content,
+    );
+  }
+}
