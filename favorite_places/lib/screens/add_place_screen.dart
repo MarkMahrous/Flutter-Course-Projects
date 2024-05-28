deacef92b87f18cfd44bcd82ea4ca8acfdbf7261
MarkMahrous <markmahrous012@gmail.com>
2024-05-28 21:47:44 +0300

    create loaction input widget to do the setup of adding location to the place using the added location package

--- favorite_places/lib/screens/add_place_screen.dart
+++ favorite_places/lib/screens/add_place_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -58,6 +59,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ImageImput(onPickImage: (File image) {
               _pickedImage = image;
             }),
+            const SizedBox(height: 10),
+            const LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
